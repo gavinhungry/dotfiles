1e89c41b01e071ac6b6c3e22b549566c34c185e8
Gavin Lloyd <gavinhungry@gmail.com>
2017-10-26 10:56:55 -0700

    Cleanup autocompletion

--- .bash.d/complete.sh
+++ .bash.d/complete.sh
@@ -1,14 +1,37 @@
-_packages() {
-  local cur
-  cur="${COMP_WORDS[COMP_CWORD]}"
-  COMPREPLY=($(compgen -W "$(pacman -Sqs); $(pacman -Qqs)" -- ${cur}))
+__commands() {
+  COMPREPLY=($(compgen -c -- "${COMP_WORDS[COMP_CWORD]}"))
 }
+complete -F __commands cw ew
+
+__users() {
+  COMPREPLY=($(compgen -u -- "${COMP_WORDS[COMP_CWORD]}"))
+}
+complete -F __users gecos
+
+__packages() {
+  COMPREPLY=($(compgen -W '$(pacman -Sqs); $(pacman -Qqs)' -- "${COMP_WORDS[COMP_CWORD]}"))
+}
+complete -F __packages pkgsource
 
 __pnames() {
   local cur prev words cword
   _init_completion || return
-  _pnames
+  _pnames "$cur"
 }
-
-complete -F _packages pkgsource
 complete -F __pnames psof
+
+__modparam() {
+  local cur prev words cword
+  _init_completion || return
+  _installed_modules "$cur"
+}
+complete -F __modparam modparam
+
+_completion_loader journalctl
+complete -F _journalctl jc
+
+_completion_loader pacman
+complete -o default -F _pacman packer
+
+_completion_loader systemctl
+complete -F _systemctl sc
