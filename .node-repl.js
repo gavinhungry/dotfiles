53dc479b85b9c16cc72c4c8d730f87c8cbb91b30
Gavin Lloyd <gavinhungry@gmail.com>
2020-01-14 10:29:36 -0800

    Add .node-repl.js

---
+++ .node-repl.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+
+const os = require('os');
+const path = require('path');
+const repl = require('repl');
+
+const CONTEXT_FILENAME = '.node-repl-context.js';
+const CONTEXT_PATH = path.join(os.homedir(), CONTEXT_FILENAME);
+
+let _repl = repl.start({
+  preview: false
+});
+
+_repl.setupHistory(process.env.NODE_REPL_HISTORY, () => {
+  Object.assign(_repl.context, {
+    _repl,
+    env: process.env
+  });
+
+  try {
+    const context = require(CONTEXT_PATH);
+    Object.assign(_repl.context, context);
+  } catch(err) {
+    // could not load local context
+  }
+});
